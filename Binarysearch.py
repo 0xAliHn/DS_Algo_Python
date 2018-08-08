210d4c629f594ca0529be8dd1bfbb0ce69595e33
0xAliHn <ali.ict06@gmail.com>
2018-08-08 23:27:05 +0600

    Add LIS nlogn solution

--- Binarysearch.py
+++ Binarysearch.py
@@ -1,5 +1,5 @@
-# O(log(n))
-def binarysearch (ar, item):
+# time O(logn) space O(logn)
+def binarysearch(ar, item):
     if len(ar) == 0:
         return False
     mid = len(ar) // 2
@@ -7,15 +7,48 @@ def binarysearch (ar, item):
         return True
     if ar[mid] < item:
         return binarysearch(ar[mid + 1:], item)
-    elif ar[mid] > item:
+    else:
         return binarysearch(ar[:mid], item)
-    return False
+
+
+# time O(log(n)) space O(1)
+def binarysearch_optimize(ar, l, r, item):
+    while l <= r:
+        m = (l + r) // 2
+        if ar[m] == item:
+            return m
+        elif ar[m] < item:
+            l = m + 1
+        else:
+            r = m - 1
+
+    return -1
+
+
+# time O(log(n)) space O(1)
+def binarysearch_more_optimize(ar, l, r, item):
+    while r - l > 1:
+        m = l + (r - l) // 2
+        if ar[m] <= item:
+            l = m
+        else:
+            r = m
+
+    if arr[l] == item:
+        return l
+    else:
+        return -1
 
 
 # Test array
 arr = [1, 2, 3, 5, 8, 22, 34, 42, 87, 103]
-x = 42
+x = 87
 
 # Function call
 result = binarysearch(arr, x)
 print(result)
+
+result = binarysearch_more_optimize(arr, 0, len(arr) - 1, x)
+print(result)
+
+# https://www.geeksforgeeks.org/the-ubiquitous-binary-search-set-1/
---
+++ LIS.py
@@ -0,0 +1,91 @@
+# Tabulation/bottom-up approach
+# O(n*n)
+def LIS(arr):
+    global i
+    arrlen = len(arr)
+    length = [1] * arrlen
+    slist = [[i] for i in arr]
+
+    for i in range(1, arrlen):
+        for j in range(i):
+            if arr[i] > arr[j] and length[i] < length[j] + 1:
+                length[i] = max(length[i], length[j] + 1)
+                slist[i] = slist[j] + [arr[i]]
+
+    # Print the subsequence
+    print(slist[i])
+
+    return length[i]
+
+
+# Binary Search
+# O(nlogn)
+def lis_otimize(arr):
+    size = len(arr)
+
+    temp = [0 for _ in range(size + 1)]
+
+    increasingSub = [-1 for _ in range(size + 1)]
+
+    length = 0
+    for i in range(1, size):
+
+        if arr[i] < arr[temp[0]]:
+
+            # new smallest value
+            temp[0] = i
+
+        elif arr[i] > arr[temp[length]]:
+
+            # arr[i] wants to extend
+            # largest subsequence
+            increasingSub[i] = temp[length]
+            temp[length + 1] = i
+            length += 1
+
+        else:
+
+            # arr[i] wants to be a
+            # potential condidate of
+            # future subsequence
+            # It will replace ceil
+            # value in temp
+            pos = FindIndex(arr, temp, -1,
+                            length, arr[i])
+
+            increasingSub[i] = temp[pos - 1]
+            temp[pos] = i
+
+    print(temp)
+    print(increasingSub)
+    i = temp[length]
+    while i >= 0:
+        print(arr[i], " ", end="")
+        i = increasingSub[i]
+    print()
+
+    return length + 1
+
+
+# Binary search
+def FindIndex(arr, T, l, r, key):
+    while r - l > 1:
+        m = l + (r - l) // 2
+        if arr[T[m]] <= key:
+            l = m
+        else:
+            r = m
+
+    return r
+
+
+arr = [10, 22, 9, 33, 21, 50, 41, 60]
+print("1. Length of lis is", LIS(arr))
+print("2. Length of lis is", lis_otimize(arr))
+
+# Ref
+# https://www.geeksforgeeks.org/longest-increasing-subsequence/
+# https://www.youtube.com/watch?v=E6us4nmXTHs     //For finding length
+# https://www.youtube.com/watch?v=SZByPn0deMY     //For printing sequence
+# https://www.youtube.com/watch?v=S9oUiVYEq7E     //For nlogn solution
+# https://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
